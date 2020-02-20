7c41b199af66c9c537d6d06135329a919dd316dd
Koungoins <bina.ansuf@gmail.com>
2020-02-20 12:03:23 +0000

    Entetes

--- fonctions_tris.py
+++ fonctions_tris.py
@@ -1,3 +1,6 @@
+#!/bin/env python
+# coding=utf-8
+
 from labels import *
 import time
 
--- frame_recherche.py
+++ frame_recherche.py
@@ -1,4 +1,6 @@
+#!/bin/env python
 # coding=utf-8
+
 from tkinter import *
 
 #Fichier contenant tout les textes
