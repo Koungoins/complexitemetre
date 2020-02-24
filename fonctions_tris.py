c1c5ff1c20882c69122dfcb8b89433b7a923937e
Ansuf B <bina.ansf@gmail.com>
2020-02-24 19:39:09 +0300

    Voir erreur tri fusion

--- fonctions_tris.py
+++ fonctions_tris.py
@@ -10,60 +10,112 @@ import variables_g as varsG
 import statistics as stats
 
 
-def triSelection(tab) :
-  for i in range(len(tab)):
-    # Trouver le min
-    min = i
-    for j in range(i+1, len(tab)):
-      if tab[min] > tab[j]:
-        min = j
-    #deplace le min
-    tmp = tab[i]
-    tab[i] = tab[min]
-    tab[min] = tmp
-  return tab
+def triSelection(tabl) :
+  tmp = 0
+  jMin = 0
+  j = 0
+  for i in range(len(tabl)-1) :
+    j = i
+    jMin = j
+    #Recherche du min
+    while j < len(tabl) :
+      if tabl[j]<tabl[jMin] :
+        jMin = j
+      j = j + 1
+
+    #Echange de place si min different de i
+    if jMin != i :
+      tmp = tabl[i]
+      tabl[i]=tabl[jMin]
+      tabl[jMin] = tmp
+      #print("Liste:", liste)
+  return tabl
 
 
 
-def triInsertion(tab) :
-  # Parcour de 1 à la taille du tab
-  for i in range(1, len(tab)):
-    k = tab[i]
-    j = i-1
-    while j >= 0 and k < tab[j] :
-      tab[j + 1] = tab[j]
-      j -= 1
-    tab[j + 1] = k
-  return ta
+def triInsertion(liste) :
+  i = 1
+  iTemp = 0
+  j = 0
+  n = 0
+  while i < len(liste) :
+    iTemp = liste[i]
+    #print("iTemp:",iTemp)
+    j = i - 1
+    #Recherche du plus petit
+    while j >= 0 :
+      #print("recherche:",j)
+      if liste[j] < iTemp :
+        j = j + 1
+        petit = True
+        break
+      j = j - 1
+    #Pas de plus petit, on met au début
+    if j < 0 :
+      j = 0
+    #print("Place:",liste[j])
+
+    #Deplacement
+    n = i
+    while n >= j :
+      #print("n:",n,", j:",j)
+      liste[n] = liste[n - 1]
+      #print("dep:",liste)
+      n = n - 1
+    #print("iTemp repl:",iTemp)
+    #Mise en tete
+    liste[j] = iTemp
+    #print("Liste nouv :",liste)
+    i = i + 1
+  return liste
 
 
-def triBulle(tab) :
-  n = len(tab)
-  # Traverser tous les éléments du tableau
-  for i in range(n):
-    for j in range(0, n-i-1):
-      # échanger si l'élément trouvé est plus grand que le suivant
-      if tab[j] > tab[j+1] :
-        tab[j], tab[j+1] = tab[j+1], tab[j]
-  return tab
+def triBulle(tabl) :
+  tmp = 0
+  changement = False
+  for i in range(len(tabl)-1) :
+    if tabl[i] > tabl[i+1] :
+      tmp = tabl[i]
+      tabl[i] = tabl[i+1]
+      tabl[i+1] = tmp
+      changement = True
+  if changement :
+    triBulle(tabl)
+  return tabl
 
 
-def triRapide(tab) :
-  i1, i2, n1, n2 = 0, 0, len(t1), len(t2)
-  t = []
-  while i1 < n1 and i2 < n2 :
-    if t1[i1] < t2[i2] :
-      t.append(t1[i1])
-      i1 += 1
+def fusion(a, b) :
+    c = []
+    n = len(a)
+    m = len(b)
+    i = 0
+    j = 0
+    while i < len(a) and j < len(b) : # on continue tant que les deux listes ne sont pas vides
+    	if a[i] < b[j] :
+            c.append(a[i])
+            i = i + 1
+    	else :
+            c.append(b[j])
+            j = j + 1
+    # ‡ ce stade, une (au moins) des deux listes est vide, et on ajoute les ÈlÈments de líautre liste ‡ c
+    if i == len(a) :
+    	for j in range(j,len(b)) :
+            print("ici 2")
+            c.append(b[j])
     else :
-      t.append(t2[i2])
-    i2 += 1
-  if i1 == n1 :
-    t.extend(t2[i2:])
-  else :
-    t.extend(t1[i1:])
-  return t
+    	for j in range(i,len(a)) :
+            print("ici 3")
+            c.append(a[i])
+    return c
 
+def triRapide(a) : # il síagit ici díune fonction renvoyant la liste des ÈlÈments de a triÈs
+    print("ici 11")
+    n = len(a)
+    if n <= 1 :
+        return a
+    m = n // 2
+    # on utilise ensuite le ìslicingî pour dÈÖnir les sous-listes, on les trie et on renvoie leur fusion :
+    return fusion( triFusion(a[0:m]) , triFusion(a[m:n]) )
 
 
 #fonction qui execute les mesures
@@ -90,10 +142,11 @@ def runMesure(selection) :
                 triSelection(varsG.data[d])
                 found = True
             elif sel == nom_tri_insertion :
-                triSelection(varsG.data[d])
+                triInsertion(varsG.data[d])
                 found = True
             elif sel == nom_tri_rapide :
-                triSelection(varsG.data[d])
+                print("Debut fonction")
+                triRapide(varsG.data[d])
                 found = True
 
             t = time.time() - t
--- variables_g.py
+++ variables_g.py
@@ -12,7 +12,7 @@ choix_fonctions = [nom_somme, nom_factoriel, nom_rechercher, nom_tris, nom_fibo]
 choix_fonction_selected = nom_somme
 
 #Liste des données
-dico = {'keys':[1, 10, 20, 50, 70, 100, 150, 200, 300, 500, 700, 800, 1000, 1500, 3000, 3500, 4000, 10000]}
+dico = {'keys':[1, 10, 20, 50, 70, 100, 150, 200, 300]}#, 500, 700, 800, 1000, 1500, 3000, 3500, 4000, 10000]}
 
 keys = dico['keys']
 keys_utiles = []
